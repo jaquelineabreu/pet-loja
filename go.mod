517bc120f40a2d34bd96efc3f49614ecb380b5b7
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2023-02-07 16:19:53 -0300

    Criada a conexao com o banco de dados

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/jaquelineabreu/pet-loja
+
+go 1.20
+
+require github.com/lib/pq v1.10.7 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
 	"text/template"
+
+	_ "github.com/lib/pq"
 )
+
+func conectaComBancoDeDados() *sql.DB	{
+	conexao := "user=postgres dbname=pet_loja password= host=localhost sslmode=disable"
+	db, err  := sql.Open("postgres", conexao)
+	if err != nil{
+		panic(err.Error())
+	}
+
+	return db
+}
 
 type Produto struct{
 	Nome string
@@ -15,8 +28,11 @@ type Produto struct{
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main(){
+	db := conectaComBancoDeDados()
+	defer db.Close()
 	http.HandleFunc("/",index)
 	http.ListenAndServe(":8000", nil)
+
 }
 
 func index(w http.ResponseWriter, r *http.Request){
