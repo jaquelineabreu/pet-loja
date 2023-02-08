8c7626f0004a28c6b9e81527de45eefe7c71f869
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2023-02-08 16:19:27 -0300

    salvando o produto no banco

--- controllers/produtos.go
+++ controllers/produtos.go
@@ -2,7 +2,9 @@ package controllers
 
 import (
 	"html/template"
+	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/jaquelineabreu/pet-loja/models"
 )
@@ -11,10 +13,39 @@ var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func Index(w http.ResponseWriter, r *http.Request){
 	todosOsProdutos := models.BuscaTodosOsProdutos()
-	temp.ExecuteTemplate(w, "Index", todosOsProdutos)
-	
+	temp.ExecuteTemplate(w, "Index", todosOsProdutos)	
 }
 
 func New(w http.ResponseWriter, r *http.Request){
 	temp.ExecuteTemplate(w, "New", nil)
 }
+
+func Insert(w http.ResponseWriter, r *http.Request){
+	if r.Method == "POST"{
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")	
+		
+		precoConvertido, err := strconv.ParseFloat(preco, 64)
+
+		if err != nil{
+			log.Println("Erro na conversão do preço: ", err)
+		}
+
+		quantidadeConvertida, err := strconv.Atoi(quantidade)
+
+		if err != nil{
+			log.Println("Erro na conversão da quantidade: ", err)
+		}
+
+
+		models.CriaNovoProduto(nome, descricao, precoConvertido, quantidadeConvertida)
+
+		http.Redirect(w, r, "/", 301)
+
+	}
+
+	http.Redirect(w,r, "/", 301)
+}
+
--- models/produtos.go
+++ models/produtos.go
@@ -43,3 +43,17 @@ func BuscaTodosOsProdutos () []Produto{
 	defer db.Close()
 	return produtos
 }
+
+func CriaNovoProduto(nome, descricao string, preco float64, quantidade int){
+	db := db.ConectaComBancoDeDados()
+
+	insereDadosNoBanco, err := db.Prepare("insert into produtos(nome, descricao, preco, quantidade) values ($1, $2, $3, $4)")
+
+	if err != nil{
+		panic(err.Error())
+	}
+
+	insereDadosNoBanco.Exec(nome, descricao, preco, quantidade)
+	defer db.Close()
+
+}
