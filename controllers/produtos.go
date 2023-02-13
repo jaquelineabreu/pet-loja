30d08725dddf515ead0d255d85d03867832fa059
Jaqueline Abreu <jaquelineabreuvr@gmail.com>
2023-02-13 15:45:58 -0300

    atualizando dados

--- controllers/produtos.go
+++ controllers/produtos.go
@@ -57,6 +57,39 @@ func Delete(w http.ResponseWriter, r *http.Request){
 
 
 func Edit(w http.ResponseWriter, r *http.Request){
-	temp.ExecuteTemplate(w, "Edit", nil)
+	idDoProduto := r.URL.Query().Get("id")
+	produto := models.EditaProduto(idDoProduto)
+	temp.ExecuteTemplate(w, "Edit", produto)
+}
+
+func Update(w http.ResponseWriter, r *http.Request){
+	if r.Method == "POST" {
+		id := r.FormValue("id")
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+
+		idConvertidaParaInt, err := strconv.Atoi(id)
+		if err != nil{
+			log.Println("Erro na conversão do id para int: ", err)
+		}
+
+		precoConvertidaParaFloat, err := strconv.ParseFloat(preco, 64)
+		if err != nil{
+			log.Println("Erro na conversão do preço para float: ", err)
+		}
+
+		quantidadeConvertidaParaInt, err := strconv.Atoi(quantidade)
+		if err != nil{
+			log.Println("Erro na conversão da quantidade para int: ", err)
+		}
+
+		models.AtualizaProduto(idConvertidaParaInt, nome, descricao, precoConvertidaParaFloat, quantidadeConvertidaParaInt)
+		
+	
+	}
+
+	http.Redirect(w, r, "/", 301)
 }
 
